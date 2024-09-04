0de0b9fb2fa5428469c3e29b68111bfe178920ce
al mahfud muhamad bakri <almahfudbakri@gmail.com>
2024-09-04 10:52:39 +0700

    PHP Date And Time

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
 echo"belajar php dan git";
+
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
