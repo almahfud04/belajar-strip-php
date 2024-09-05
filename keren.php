93faa69692932e72f1b7271218e76af348b800d4
al mahfud muhamad bakri <almahfudbakri@gmail.com>
2024-09-05 09:16:53 +0700

    PHP Date And Time

---
+++ keren.php
@@ -0,0 +1,9 @@
+<?php
+echo"belajar php dan git";
+
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+
+?>
